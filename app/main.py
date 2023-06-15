00e0e992dcae4c11f5eee26a7e32b8b9e574441b
Juan <juaoose@users.noreply.github.com>
2023-06-15 14:55:17 -0500

    chore: disable lm

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from encodec import EncodecModel
 from encodec.compress import compress
 from encodec.utils import convert_audio
 
+# HQ model
 model = EncodecModel.encodec_model_48khz()
 model.set_target_bandwidth(6.0)
 
@@ -22,12 +23,11 @@ def encode(file: UploadFile = File(...)):
     wav, sr = torchaudio.load(io.BytesIO(contents))
     wav = convert_audio(wav, sr, model.sample_rate, model.channels)
 
-    # We should be able to use lm, if we run on GPU
-    audio_bytes = compress(model, wav, True)
+    audio_bytes = compress(model, wav, False)
 
     with open("test.ecdc", 'wb') as f:
         f.write(audio_bytes)
 
     # TODO(juaoose): might want to use StreamingResponse
     # once we support chunked encoding
-    return FileResponse("test.ecdc")
+    return FileResponse("test.ecdc", media_type="application/octet-stream", filename="response.ecdc")
